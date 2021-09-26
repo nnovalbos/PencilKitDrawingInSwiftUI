10229cbd0818399567f9af820c189b94be506726
Nicolás Novalbos <nicolas.novalbos@gmail.com>
2021-09-26 09:04:05 +0200

    Initial Commit

---
+++ PencilKitDrawingInSwiftUI/PencilKitDrawingInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilKitDrawingInSwiftUIApp.swift
+//  PencilKitDrawingInSwiftUI
+//
+//  Created by Nicolas Novalbos on 26/9/21.
+//
+
+import SwiftUI
+
+@main
+struct PencilKitDrawingInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
